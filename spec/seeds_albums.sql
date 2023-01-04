df4e401fb31c810698e068881164efd8211eb150
josh-p-git <joshua.penney91@gmail.com>
2023-01-04 15:21:18 +0000

    album and artists complete

---
+++ spec/seeds_albums.sql
@@ -0,0 +1,4 @@
+TRUNCATE TABLE albums RESTART IDENTITY;
+
+INSERT INTO albums (title, release_year) VALUES('Doolittle', '1989');
+INSERT INTO albums (title, release_year) VALUES('Surfer Rosa', '1988');
